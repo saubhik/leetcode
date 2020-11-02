7b7005a9224fc31a4b6d5d8f9dbd62f2a654764f
saubhik <saubhik.mukherjee@gmail.com>
2020-11-02 10:11:41 +0900

    solve Number of Longest Increasing Subsequence

--- problems/number_of_longest_increasing_subsequence.py
+++ problems/number_of_longest_increasing_subsequence.py
@@ -37,3 +37,78 @@ class Solution:
                 ans += count[i]
 
         return ans
+
+
+class SolutionSegmentTree:
+    """
+    Time Complexity: O(nlgn).
+    Space Complexity: O(n).
+    """
+
+    class Node:
+        def __init__(self, range_left: int, range_right: int):
+            self.range_left = range_left
+            self.range_right = range_right
+            self._left = None
+            self._right = None
+            self.val = 0, 1
+
+        @property
+        def range_mid(self) -> int:
+            return (self.range_left + self.range_right) // 2
+
+        @property
+        def left(self):
+            if self._left is None:
+                self._left = self.__class__(
+                    range_left=self.range_left, range_right=self.range_mid
+                )
+            return self._left
+
+        @property
+        def right(self):
+            if self._right is None:
+                self._right = self.__class__(
+                    range_left=self.range_mid + 1, range_right=self.range_right
+                )
+            return self._right
+
+    @staticmethod
+    def merge(val1, val2):
+        if val1[0] == val2[0]:
+            if val1[0] == 0:
+                return 0, 1
+            return val1[0], val1[1] + val2[1]
+        return max(val1, val2)
+
+    def query(self, node: Node, key: int):
+        if node.range_right <= key:
+            return node.val
+        elif key < node.range_left:
+            return 0, 1
+        else:
+            return self.merge(
+                val1=self.query(node=node.left, key=key),
+                val2=self.query(node=node.right, key=key),
+            )
+
+    def insert(self, node: Node, key: int, val: tuple):
+        if node.range_left == node.range_right:
+            node.val = self.merge(val1=node.val, val2=val)
+            return
+        if key <= node.range_mid:
+            self.insert(node=node.left, key=key, val=val)
+        else:
+            self.insert(node=node.right, key=key, val=val)
+        node.val = self.merge(val1=node.left.val, val2=node.right.val)
+
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        root = self.Node(range_left=min(nums), range_right=max(nums))
+        for num in nums:
+            # O(lg n) operation.
+            length, count = self.query(node=root, key=num - 1)
+            # O(lg n) operation.
+            self.insert(node=root, key=num, val=(length + 1, count))
+        return root.val[1]
--- tests/test_number_of_longest_increasing_subsequence.py
+++ tests/test_number_of_longest_increasing_subsequence.py
@@ -1,11 +1,13 @@
 import unittest
 
-from number_of_longest_increasing_subsequence import Solution
+from number_of_longest_increasing_subsequence import Solution, SolutionSegmentTree
 
 
 class TestNumberOfLongestIncreasingSubsequence(unittest.TestCase):
     def test_example_1(self):
         assert Solution().findNumberOfLIS(nums=[1, 3, 5, 4, 7]) == 2
+        assert SolutionSegmentTree().findNumberOfLIS(nums=[1, 3, 5, 4, 7]) == 2
 
     def test_example_2(self):
         assert Solution().findNumberOfLIS(nums=[2, 2, 2, 2, 2]) == 5
+        assert SolutionSegmentTree().findNumberOfLIS(nums=[2, 2, 2, 2, 2]) == 5
