e6d31183009f027cadd12b89c39217b42296b24f
saubhik <saubhik.mukherjee@gmail.com>
2020-11-08 11:14:04 +0900

    solve Add Two Numbers II with constant space

--- problems/add_two_numbers_ii.py
+++ problems/add_two_numbers_ii.py
@@ -34,3 +34,52 @@ class Solution:
             node = ListNode(val=val, next=node)
 
         return node
+
+
+class OfficialSolution:
+    def get_length(self, node: ListNode) -> int:
+        length = 0
+        while node:
+            length += 1
+            node = node.next
+        return length
+
+    # Time Complexity: O(n1 + n2).
+    # Space Complexity: O(1).
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        n1, n2 = self.get_length(node=l1), self.get_length(node=l2)
+
+        # Does l1 point to the first node now? Yes.
+        node = None
+        while n1 or n2:
+            num1 = 0
+            if n1 >= n2:
+                num1 = l1.val
+                l1 = l1.next
+                n1 -= 1
+
+            num2 = 0
+            if n2 > n1:
+                num2 = l2.val
+                l2 = l2.next
+                n2 -= 1
+
+            # Don't care about carry for now.
+            node = ListNode(val=num1 + num2, next=node)
+
+        # Reverse the list & care about the carry.
+        #         7 -> 2 -> 4  -> 3 -> None
+        #              5 -> 6  -> 4 -> None
+        # None <- 7 <- 7 <- 10 <- 7
+        head, carry = None, 0
+        while node:
+            remaining = node.val + carry
+            new_val, carry = remaining % 10, remaining // 10
+            node.val = new_val
+
+            head, node.next, node = node, head, node.next
+
+        if carry:
+            head = ListNode(val=carry, next=head)
+
+        return head
--- tests/test_add_two_numbers_ii.py
+++ tests/test_add_two_numbers_ii.py
@@ -1,20 +1,21 @@
 from unittest import TestCase
 
-from add_two_numbers_ii import Solution, ListNode
+from add_two_numbers_ii import Solution, ListNode, OfficialSolution
 
 
 class TestAddTwoNumbersII(TestCase):
     def test_example_1(self):
-        l1 = ListNode(
-            val=7, next=ListNode(val=2, next=ListNode(val=4, next=ListNode(val=3)))
-        )
+        for solution in (Solution(), OfficialSolution()):
+            l1 = ListNode(
+                val=7, next=ListNode(val=2, next=ListNode(val=4, next=ListNode(val=3)))
+            )
 
-        l2 = ListNode(val=5, next=ListNode(val=6, next=ListNode(val=4)))
+            l2 = ListNode(val=5, next=ListNode(val=6, next=ListNode(val=4)))
 
-        l3 = Solution().addTwoNumbers(l1=l1, l2=l2)
+            l3 = solution.addTwoNumbers(l1=l1, l2=l2)
 
-        assert l3.val == 7
-        assert l3.next.val == 8
-        assert l3.next.next.val == 0
-        assert l3.next.next.next.val == 7
-        assert l3.next.next.next.next is None
+            assert l3.val == 7
+            assert l3.next.val == 8
+            assert l3.next.next.val == 0
+            assert l3.next.next.next.val == 7
+            assert l3.next.next.next.next is None
