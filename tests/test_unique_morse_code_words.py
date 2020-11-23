e9ca86b15d0eac1005df64f986afbe093f16a9eb
saubhik <saubhik.mukherjee@gmail.com>
2020-11-23 11:38:07 +0900

    solve Unique Morse Code Words

--- tests/test_unique_morse_code_words.py
+++ tests/test_unique_morse_code_words.py
@@ -6,7 +6,6 @@ from unique_morse_code_words import Solution
 class TestUniqueMorseCodeWords(TestCase):
     def test_example_1(self):
         assert (
-                Solution().uniqueMorseRepresentations(
-                    words=["gin", "zen", "gig", "msg"])
-                == 2
+            Solution().uniqueMorseRepresentations(words=["gin", "zen", "gig", "msg"])
+            == 2
         )
