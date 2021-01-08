7f273503222fc483f4831f58f30f7b3447712ea9
saubhik <saubhik.mukherjee@gmail.com>
2021-01-08 11:02:17 +0900

    Kth Missing Positive Number

--- problems/kth_missing_positive_number.py
+++ problems/kth_missing_positive_number.py
@@ -19,9 +19,45 @@ class Solution:
         return last + k
 
 
+class SolutionTwo:
+    """
+    Binary Search.
+    Time Complexity: O(logn).
+    Space Complexity: O(1).
+    """
+
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        n = len(arr)
+        lo, hi = 0, n - 1
+        while lo < hi - 1:
+            mid = hi - (hi - lo) // 2
+            # Number of missing elements before arr[mid] = arr[mid] - mid - 1
+            missing = arr[mid] - (mid + 1)
+            if missing >= k:
+                hi = mid
+            else:
+                lo = mid
+
+        # Now we have an interval [lo,lo+1]
+        missing_left = arr[lo] - (lo + 1)
+        missing_right = arr[hi] - (hi + 1)
+
+        if missing_left >= k:
+            # On left of interval.
+            return k
+        elif missing_right < k:
+            # On right of interval.
+            return arr[hi] + k - missing_right
+        else:
+            # In the interval.
+            return arr[lo] + k - missing_left
+
+
 class TestFindKthPositive(TestCase):
     def test_example_1(self):
         assert Solution().findKthPositive(arr=[2, 3, 4, 7, 11], k=5) == 9
+        assert SolutionTwo().findKthPositive(arr=[2, 3, 4, 7, 11], k=5) == 9
 
     def test_example_2(self):
         assert Solution().findKthPositive(arr=[1, 2, 3, 4], k=2) == 6
+        assert SolutionTwo().findKthPositive(arr=[1, 2, 3, 4], k=2) == 6
